b6454e02ed254bf224320a89b353c2a8692d13c0
Chinnawon6812022610247 <s6812022610247@email.kmutnb.ac.th>
2025-07-21 15:40:57 +0700

    ComWk05_3

---
+++ ComWk05_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+printf("a =\t\t0\t.\t6\t1\t7\t%%\t\n");
+printf("b =\t\t\t2\t5\t%%\t\t\t\n");;
+printf("c \t=\t1\t9\t.\t0\t0\t0\t%%\n");
+printf("d \t\t=\t9\t\t%%\t\t\t\n");
+printf("e =\t1\t2\t4\t.\t5\t5\t5\t%%\n");
+printf("f =\t0\t.\t0\t0\t1\t%%\t\t\n");
+return 0;
+}
